a7ccd2e633e0a4dd320f7cb0177705d5ddd5afc8
ygl <ygl@example.com>
2019-05-23 11:27:47 +0800

    my first stack

---
+++ first_Stack/MyFirstStack.py
@@ -0,0 +1,33 @@
+"""
+我的第一个栈
+"""
+class MyFirstStack(object):
+	"""docstring for MyFirstStack"""
+	def __init__(self):
+		self.items = []
+
+	def is_empty(self):
+		""" 判断栈是否为空"""
+		return self.items == []
+
+	def push(self,item):
+		"""像栈顶添加一个元素"""
+		return self.items.append(item)
+
+	def pop(self):
+		""" 向栈顶删除一个元素"""
+		return self.items.pop()
+
+	def peek(self):
+		""" 找到栈的最后一个元素 """
+		last = len(self.items) - 1
+		return self.items[last]
+
+	def size(self):
+		return len(self.items)
+
+#新建的栈是空的
+MyFirstStack = MyFirstStack()
+print(MyFirstStack.is_empty())
+
+
