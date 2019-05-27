abae1759ca90f370614d9d9989eceb26aaaeb726
ygl <ygl@example.com>
2019-05-27 11:52:42 +0800

    用len()函数获取list 列表的元素个数

--- mylist.py
+++ mylist.py
@@ -9,3 +9,10 @@ print(fruit1)
 print("第二种方法创建一个列表：")
 fruit2 = []
 print(fruit2)
+
+#用len()函数获取list 列表的元素个数
+fruit2 = [0, 1, 2, 3, 4]
+print("列表fruit2：")
+print(fruit2)
+print("列表fruit2的元素个数：")
+print(len(fruit2))
