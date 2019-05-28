1664e6d9ead86fa552edb161ef83fa7c4c392d6f
ygl <ygl@example.com>
2019-05-28 17:16:13 +0800

    Django模板

--- myDjango/HelloWorld/HelloWorld/view.py
+++ myDjango/HelloWorld/HelloWorld/view.py
@@ -1,5 +1,7 @@
 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
-	return HttpResponse("hello world !")
+	context = {}
+	context['hello'] = 'Hello world!!'
+	return render(request,'hello.html',context)
