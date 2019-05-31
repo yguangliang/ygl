616c5e299637e5afca7ca233d0998adfc10e3aee
ygl <ygl@example.com>
2019-05-31 09:21:56 +0800

    实现博客数据返回页面

--- myDjango/django_introduction/blog/urls.py
+++ myDjango/django_introduction/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+import blog.views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path('content', blog.views.article_content),
 ]
--- myDjango/django_introduction/blog/views.py
+++ myDjango/django_introduction/blog/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
+
+from blog.models import  Article
+
 # 视图处理的地方
 # Create your views here.hello_world视图函数来返回字符串helloworld
 
 
 def hello_world(request):
     return HttpResponse("hello world!")
+
+
+def article_content(request):
+    article = Article.objects.all()[0]
+    title = article.title
+    brief_content = article.brief_content
+    content = article.article_id
+    publish_date = article.publish_date
+    return_str = "title:%s,brief_content:%s," \
+                 "content:%s,publish_date:%s" % (title, brief_content, content, publish_date)
+
+    return HttpResponse(return_str)
